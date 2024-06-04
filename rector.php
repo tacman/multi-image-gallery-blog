b201471af617ef6ae649d5bff99553e956b463a4
Tac Tacelosky <tacman@gmail.com>
2024-06-04 08:32:27 -0400

    bump to 6.4

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+  ->withSets([
+      DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+      SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+  ])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
